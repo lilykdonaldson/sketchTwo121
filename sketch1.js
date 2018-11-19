99769b715b4ad65a986250c9cde4e0e8f37c0e9c
Lily Donaldson <lilykdonaldson@gmail.com>
2018-11-19 02:10:31 -0500

    test 1/3

--- sketch1.js
+++ sketch1.js
@@ -1,8 +1,8 @@
 function setup(){
-    var newcanvas = createCanvas(windowWidth/3, 500);
+    var newcanvas = createCanvas(629, 400);
     newcanvas.parent("sketch2css");
     background(30, 196, 0);
 }
 function draw(){
-	ellipse(100,200,30,30);
+	ellipse(mouseX,mouseY,30,30);
 }

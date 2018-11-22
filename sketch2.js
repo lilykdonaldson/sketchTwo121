f37ea815ecb05cbb82a48ffbd7ac685fa6b46494
Lily Donaldson <lilykdonaldson@gmail.com>
2018-11-22 14:08:28 -0600

    Update sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -1,6 +1,11 @@
 function setup(){
+<<<<<<< HEAD
     createCanvas(400, 350);
     background(241,213,204));
+=======
+    createCanvas(314, 350);
+    background(30, 196, 0);
+>>>>>>> parent of fe0e977... Update sketch2.js
 }
 function draw(){
 	ellipse(mouseX,mouseY,30,30);
